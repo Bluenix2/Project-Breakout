9bbd5fb59fb2ee81ab326eb24c5680e931dfb6c3
Bluenix <gabriel.nordholm@gmail.com>
2020-03-08 12:50:12 +0100

    Add Camera2D, and add code to make a responsive screen

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,27 @@
 extends Node
 
+onready var viewport = get_viewport()
+onready var game_size = Vector2(
+	ProjectSettings.get_setting("display/window/size/width"),
+	ProjectSettings.get_setting("display/window/size/height"))
+
+func _ready():
+	viewport.connect("size_changed", self, "resize_window")
+	resize_window()
+
+func resize_window():
+	var new_size = OS.get_window_size()
+	var scale_factor
+
+	if new_size.x < game_size.x:
+		scale_factor = game_size.x/new_size.x
+		new_size = Vector2(game_size.x, new_size.y*scale_factor)
+	if new_size.y < game_size.y:
+		scale_factor = game_size.y/new_size.y
+		new_size = Vector2(new_size.x*scale_factor, game_size.y)
+	
+	viewport.set_size_override(true, new_size)
+
 func _process(delta):
 	process_platform(delta)
 
