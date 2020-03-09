87d312bcdeb89987300a7822cf4d3d2e6072ecbd
Bluenix <gabriel.nordholm@gmail.com>
2020-03-09 16:12:55 +0100

    Add Ball, and Brick collision handling

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,8 +1,15 @@
 extends RigidBody2D
+class_name Ball
 
 export (int) var speed
 
+func _ready():
+	set_linear_velocity(Vector2(500, 500))
+
 func _on_collision(body):
-	if body == $"../Platform":
+	if body is Platform:
 		set_linear_velocity(
 			body.position.direction_to(position)*linear_velocity.length())
+	
+	if body is Brick:
+		body.collided()
