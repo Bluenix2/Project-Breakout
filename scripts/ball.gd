bf96dc98de376067d1fc5c0dfa38f8e29746f912
Bluenix <gabriel.nordholm@gmail.com>
2020-03-10 14:38:42 +0100

    Add event to remove ball after exiting screen

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,8 +3,14 @@ class_name Ball
 
 export (int) var speed
 
+onready var screen_height = ProjectSettings.get_setting("display/window/size/height")
+
 func _ready():
 	set_linear_velocity(Vector2(500, 500))
+
+func _process(delta):
+	if position.y > screen_height:
+		queue_free()
 
 func _on_collision(body):
 	if body is Platform:
